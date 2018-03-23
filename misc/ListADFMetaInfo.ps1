7649fde4c68e62310f2d5a4d82806ea99825c0fd
Anil K <anil.kask@gmail.com>
2018-03-22 22:58:48 -0500

    Add files via upload

---
+++ misc/ListADFMetaInfo.ps1
@@ -0,0 +1,26 @@
+# ./ListADFMetaInfo
+# Parameters - fill these in before running the script!
+# =====================================================
+
+$rg = "<resourceGroupName>" #Resource Group Name
+$df = "<datafactoryName>" #Data Factory Name
+$pl = "<pipelineName>" #PipeLine Name
+$sb = "<subscriptionId>" #subscription
+$cred_file = "/<folder>/azure_cred.json"
+$ds_folder = "/<folder>/Datasets/" #Dataset root folder
+
+# Edit filter condition
+# Follow Dataset file naming convention to extra useful meta data by applying 
+# filters for ex. *_sqldb_* to extract SQLDB table names
+
+$filter_cond = "<filter_pattern>" 
+
+#Auto Login
+Select-AzureRmProfile -Path $cred_file
+
+Set-AzureRMContext -SubscriptionId $sb
+
+foreach ($file in Get-ChildItem $ds_folder -Recurse -Filter $filter_cond)
+{
+	Write-Output $file.BaseName
+}
